6eeb08f0ff68fb23c83f0ac4bf606117e3af8dd2
Roberto Isla <robertoisla92@gmail.com>
2021-11-28 21:17:06 -0300

    Creación de interfaz Concepto, componentes de páginas y update de paquetes, no se ha instalado ninguno

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DetalleConceptoComponent } from './pages/detalle-concepto/detalle-concepto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    DetalleConceptoComponent
   ],
   imports: [
     BrowserModule
