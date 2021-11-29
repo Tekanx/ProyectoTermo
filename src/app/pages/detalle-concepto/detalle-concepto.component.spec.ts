1dbc58097b8b2bf88d8a3a1873459419dac09aba
Roberto Isla <robertoisla92@gmail.com>
2021-11-28 21:17:43 -0300

    Creación con ng generate

---
+++ src/app/pages/detalle-concepto/detalle-concepto.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DetalleConceptoComponent } from './detalle-concepto.component';
+
+describe('DetalleConceptoComponent', () => {
+  let component: DetalleConceptoComponent;
+  let fixture: ComponentFixture<DetalleConceptoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DetalleConceptoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DetalleConceptoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/detalle-concepto/detalle-concepto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-concepto',
+  templateUrl: './detalle-concepto.component.html',
+  styleUrls: ['./detalle-concepto.component.scss']
+})
+export class DetalleConceptoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
